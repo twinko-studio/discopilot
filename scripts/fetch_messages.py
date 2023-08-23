1238bfc6e722a8ff929612e07354d2c31831a7b0
tengfei <yintengfei@gmail.com>
2023-08-23 16:13:28 -0400

    add summary using hf model

--- scripts/fetch_messages.py
+++ scripts/fetch_messages.py
@@ -3,7 +3,8 @@ import discord
 from datetime import datetime, timedelta
 from discopilot.configuration_loader import ConfigurationLoader
 from discopilot.channel_mapper import ChannelMapper
-from discopilot.utils import discord_details
+from discopilot.utils import get_discord_details
+from discopilot.ai.summarizer import summarize
 
 # Read the configuration file
 config = ConfigurationLoader.load_config()
@@ -52,7 +53,7 @@ async def fetch_messages(channel_id, start_time=None, end_time=None, hours=None)
         print("No channel ID specified!")
         return
 
-    print("start fetching messages", channel_id)
+    print("start fetching channel", channel_id)
     channel = await discord_client.fetch_channel(channel_id)
     if channel is None:
         print("Channel not found!")
@@ -70,12 +71,10 @@ async def fetch_messages(channel_id, start_time=None, end_time=None, hours=None)
     else:
         print("Invalid time range!")
         return
-
-
     return messages
 
 
-def write_to_report(content, output_dir):
+def write_to_report(content, output_dir, report_type = "report"):
     if output_dir is None:
         print("No output directory specified!")
         return
@@ -89,8 +88,16 @@ def write_to_report(content, output_dir):
 
     # Create a filename with a timestamp
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_file = os.path.join(output_dir, f"report_{timestamp}.md")
-    print("Writing to file:", output_file)
+
+    if report_type == "report":
+        output_file = os.path.join(output_dir, f"report_{timestamp}.md")
+        print("Writing full report to file:", output_file)
+    elif report_type == "summary":
+        output_file = os.path.join(output_dir, f"summary_{timestamp}.md")
+        print("Writing abstract to file:", output_file)
+    else:
+        print("Invalid report type!")
+        return
     
     # Write the formatted content to a file
     with open(output_file, "w") as file:
@@ -105,17 +112,25 @@ async def generate_report(start_time=None, end_time=None, hours=24, output_dir =
     # Fetch messages from each channel in order and write to report
     report_content = ["# Table of Contents"]
     details = []
+    abstract = []
     channels = dict(config["Report"])
     report_titles = dict(config['Report_Title'])
     
     for channel_name, should_fetch in channels.items():
         if should_fetch.lower() == 'true':
             cid = cid_mapper.get_id_from_name(channel_name)
+            print("fetching messages from channel", channel_name, cid)
             msg = await fetch_messages(channel_id=cid, start_time=start_time, end_time = end_time, hours=hours)
-            anchor_name = channel_name.lower().replace('_', '')  # Convert 'AI_CID' to 'aicid'
+            anchor_name = channel_name.lower().replace('_', '') 
             section_title = f"## {report_titles[channel_name]} <a name='{anchor_name}'></a>"
             details.append(section_title)
-            details.append(msg)
+            if len(msg) > 0:    
+                details.append(msg) 
+                msg_abstract = summarize(msg)
+                abstract.append(msg_abstract)   
+                abstract.append("\n")
+            else:
+                details.append("No news found")
 
     # Add TOC links and details to the report content
     for idx, channel_name in enumerate(channels.keys()):
@@ -124,10 +139,14 @@ async def generate_report(start_time=None, end_time=None, hours=24, output_dir =
         report_content.insert(idx+1, toc_link)
     
     report_content.append("\n")
+    report_content.extend(abstract)
+    report_content.append("\n")
     report_content.extend(details)
 
     # Write to markdown report
     write_to_report("\n".join(report_content), output_dir = output_dir)
+    write_to_report("\n".join(abstract), output_dir = output_dir, report_type = "summary")
+
 
 
 @discord_client.event
